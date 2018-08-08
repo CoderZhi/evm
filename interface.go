c76b8b8c62adfc1bf754597b255f6943ac70a4d3
CoderZhi <zhi@iotex.io>
2018-08-08 10:39:14 -0700

    move directory

---
+++ interface.go
@@ -0,0 +1,75 @@
+// Copyright 2016 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package vm
+
+import (
+	"math/big"
+)
+
+// StateDB is an EVM database for full state querying.
+type StateDB interface {
+	CreateAccount(Address)
+
+	SubBalance(Address, *big.Int)
+	AddBalance(Address, *big.Int)
+	GetBalance(Address) *big.Int
+
+	GetNonce(Address) uint64
+	SetNonce(Address, uint64)
+
+	GetCodeHash(Address) Hash
+	GetCode(Address) []byte
+	SetCode(Address, []byte)
+	GetCodeSize(Address) int
+
+	AddRefund(uint64)
+	GetRefund() uint64
+
+	GetState(Address, Hash) Hash
+	SetState(Address, Hash, Hash)
+
+	Suicide(Address) bool
+	HasSuicided(Address) bool
+
+	// Exist reports whether the given account exists in state.
+	// Notably this should also return true for suicided accounts.
+	Exist(Address) bool
+	// Empty returns whether the given account is empty. Empty
+	// is defined according to EIP161 (balance = nonce = code = 0).
+	Empty(Address) bool
+
+	RevertToSnapshot(int)
+	Snapshot() int
+
+	AddLog(*Log)
+	AddPreimage(Hash, []byte)
+
+	ForEachStorage(Address, func(Hash, Hash) bool)
+}
+
+// CallContext provides a basic interface for the EVM calling conventions. The EVM EVM
+// depends on this context being implemented for doing subcalls and initialising new EVM contracts.
+type CallContext interface {
+	// Call another contract
+	Call(env *EVM, me ContractRef, addr Address, data []byte, gas, value *big.Int) ([]byte, error)
+	// Take another's contract code and execute within our own context
+	CallCode(env *EVM, me ContractRef, addr Address, data []byte, gas, value *big.Int) ([]byte, error)
+	// Same as CallCode except sender and value is propagated from parent to child scope
+	DelegateCall(env *EVM, me ContractRef, addr Address, data []byte, gas *big.Int) ([]byte, error)
+	// Create a new contract
+	Create(env *EVM, me ContractRef, data []byte, gas, value *big.Int) ([]byte, Address, error)
+}
